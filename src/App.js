32db9375e0edbec4742ed795d6f684a3f3e483f4
Yevhen Rudofylov <rudofiloveugene@gmail.com>
2022-06-08 19:07:31 +0300

    variation Images List on MUI library

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import ImagesList from "./components/ImagesList";
+
+function App() {
+  return (
+    <div className="App">
+      <ImagesList />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ImagesList.js
@@ -0,0 +1,95 @@
+import * as React from "react";
+import { useEffect, useState } from "react";
+import axios from "../helpers/axios";
+import Box from "@mui/material/Box";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const ImagesList = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [open, setOpen] = useState(false);
+  const [imageParams, setImageParams] = useState([]);
+
+  useEffect(() => {
+    setLoading(true);
+    axios.get(`/list`, { params: { page, limit: 10 } }).then((data) => {
+      setImages((prevImages) => [...prevImages, ...data]);
+      setLoading(false);
+    });
+  }, [page]);
+
+  const handleClick = () => {
+    setPage((page) => page + 1);
+  };
+
+  const handleOpen = (image) => () => {
+    setImageParams(image);
+    console.log(imageParams);
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    // setImageParams([]);
+    setOpen(false);
+  };
+
+  return (
+    <>
+      <Box sx={{ p: 2, display: "flex", justifyContent: "center" }}>
+        <ImageList
+          sx={{ width: 1000, height: 500, cursor: "pointer" }}
+          cols={2}
+          rowHeight={400}
+        >
+          {images.map((image) => (
+            <ImageListItem key={image.id} onClick={handleOpen(image)}>
+              <img
+                key={image.id}
+                src={`${image.download_url}?w=164&h=164&fit=crop&auto=format`}
+                srcSet={`${image.download_url}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+                alt={image.author}
+                loading="lazy"
+              />
+            </ImageListItem>
+          ))}
+        </ImageList>
+
+        <Dialog open={open} onClose={handleClose} params={imageParams}>
+          <DialogTitle>{"This image information"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              <>{`Author: ${imageParams.author}`}</>
+              <>{`Original width: ${imageParams.width}px`}</>
+              <>{`Original height: ${imageParams.height}px`}</>
+            </DialogContentText>
+          </DialogContent>
+          ;
+          <DialogActions>
+            <Button onClick={handleClose}>OK</Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
+
+      <Box sx={{ p: 2, display: "flex", justifyContent: "center" }}>
+        {isLoading ? (
+          <CircularProgress />
+        ) : (
+          <Button variant="contained" onClick={handleClick}>
+            Show More
+          </Button>
+        )}
+      </Box>
+    </>
+  );
+};
+
+export default ImagesList;
